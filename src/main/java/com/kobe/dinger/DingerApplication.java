a97234e95239c9b102e6bbdee6cbd43cd1b16716
kbenavente24 <kobeb@uw.edu>
2026-04-17 12:45:07 -0700

    spring boot setup

---
+++ src/main/java/com/kobe/dinger/DingerApplication.java
@@ -0,0 +1,13 @@
+package com.kobe.dinger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DingerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DingerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kobe/dinger/DingerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kobe.dinger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DingerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
